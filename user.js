82d8159f3b2716153b96aaa325c097e53698e8a6
Jim <Jim>
2025-08-19 12:27:03 -0600

    initial push

--- user.js
+++ user.js
@@ -3,6 +3,7 @@
 
 async function main() {
   const users = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await fetch("https://jsonplaceholder.typicode.com/posts?userId=${userId}")
   const usersData = await users.json();
   const userListEl = document.querySelector(".user-list");
 
@@ -10,10 +11,10 @@ async function main() {
     .map(
       (user) => `<div class="user-card">
             <div class="user-card__container">
-              <h3>${user.name}</h4>
+              <h3>${user.name}</h3>
                 <p><b>Email:</b> ${user.email}</p>
                 <p><b>Phone:</b> ${user.phone}</p>
-                <p><b>Website:</b> <a href="${user.website}" target="_blank">${user.website}</a></p>
+                <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
             </div>
           </div>`
     )
