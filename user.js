d60d29b0fe0f545b6145873013b5735948781fc2
Jim <Jim>
2025-08-19 14:59:59 -0600

    push

--- user.js
+++ user.js
@@ -3,21 +3,21 @@
 const userListEl = document.querySelector(".post-list");
 
 async function main() {
-  const users = await fetch("https://jsonplaceholder.typicode.com/users");
+  const id = localStorage.getItem("id")
   const posts = await fetch("https://jsonplaceholder.typicode.com/posts?userId=${userId}")
-  const usersData = await users.json();
-  userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("")
+  const postsData = await posts.json();
+  postListEl.innerHTML = postsData.map(post =>`<div class="post">
+      <div class="post__title">
+        ${post.title}
+      </div>
+      <p class="post__body">
+    ${post.body}
+      </p>
+    </div>`)
+    .join('')
 }
 
 main();
 
-function userHHTML (user) { return `<div class="user-card">
-            <div class="user-card__container">
-              <h3>${user.name}</h3>
-                <p><b>Email:</b> ${user.email}</p>
-                <p><b>Phone:</b> ${user.phone}</p>
-                <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
-            </div>
-          </div>`
-}
+
 
