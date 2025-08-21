fd0f8ada9de50b3705976ee1b4a0daecc4aa168d
Jim <Jim>
2025-08-21 17:11:56 -0600

    5 oclock

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:Id"
+const userList = document.querySelector('.user-list');
 
 async function main() {
   const users = await fetch("https://jsonplaceholder.typicode.com/users")
   const posts = await fetch("https://jsonplaceholder.typicode.com/posts?userId=:Id")
   const usersData = await users.json();
-  const userList = document.querySelector('.post-list')
-  userList.innerHTML = usersData
-  usersData.map((user) => `<div class="user-card">
+  userList.innerHTML = usersData.map((user) => userHTML(user)).join("")
+}
+
+main()
+
+function userHTML(user) {
+   return `<div class="user-card">
                 <div class="user-card__container">
                   <h3>${user.name}</h3>
                     <p><b>Email:</b>${user.email}</p>
@@ -15,9 +20,6 @@ async function main() {
                     <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
                 </div>
               </div>`
-        )
-        .join("")
-      
 }
 
-main();
+
