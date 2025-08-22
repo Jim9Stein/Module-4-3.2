e852938c3a9c61d42eda13a07a2950cea1bbff56
Jim <Jim>
2025-08-21 22:25:34 -0600

    last Push

--- user.js
+++ user.js
@@ -1,25 +1,34 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
-const postListEl = document.querySelector(".post-list");
 
-async function main() {
-  const id = localStorage.getItem("id")
-  const posts = await fetch("https://jsonplaceholder.typicode.com/posts?userId=${userId}")
+const postListEl = document.querySelector('.post-list');
+const id = localStorage.getItem("id")
+
+async function onSearchChange(event) {
+  const id = event.target.value;
+  renderPosts(id)
+}
+
+async function renderPosts(id) {
+  const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
   const postsData = await posts.json()
-  console.log(postsData)
+  postListEl.innerHTML = postsData.map(post => postHTML(post)).join('')
+}
   
-  postListEl.innerHTML = postsData.map(post =>`<div class="post">
+function postHTML(post) {
+  return
+  `<div class="post">
       <div class="post__title">
         ${post.title}
       </div>
       <p class="post__body">
     ${post.body}
       </p>
-    </div>`)
-    .join('')
+    </div>`
+  
 }
 
-main();
+renderPosts(id);
 
 
 
